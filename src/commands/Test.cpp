5385289b9ffe875108a156a8910f0e3cb20dc154
Gabriele Canesi <gabrielecanesi999@gmail.com>
2023-07-26 21:26:53 +0200

    Added further tests

--- src/commands/Test.cpp
+++ src/commands/Test.cpp
@@ -6,6 +6,7 @@
 #include "CliCommand.h"
 #include "../algorithms.h"
 #include <iostream>
+#include <fstream>
 
 Test::Test(const std::shared_ptr<std::string> &reference, const std::shared_ptr<std::string> &r, size_t k,
            size_t firstK, double bloomFilterThreshold, double firstThreshold) :
@@ -14,37 +15,101 @@ Test::Test(const std::shared_ptr<std::string> &reference, const std::shared_ptr<
 
 
 void Test::run() {
+    auto originalReference = std::make_shared<std::string>(*reference);
+    auto originalRead = std::make_shared<std::string>(*r);
+    auto readLengths = {1000, 10000, 50000, 100000, 500000, 1000000, 2000000, 3000000, 4000000, 5000000, 6000000, 7000000, 8000000, 9000000, 10000000};
+    auto referenceLengths = {100000, 500000, 1000000, 5000000, 10000000, 20000000, 30000000, 40000000,
+                             50000000, 60000000, 70000000, 80000000, 90000000, 100000000, 120000000, 140000000, 160000000, 180000000, 200000000, (int) reference->length()};
 
-    auto length = 60000000;
-    *r = reference->substr(reference->length() / 2, length);
+    std::ofstream file("timingsR.csv");
+    file << "read,reference,time\n";
+    for (auto lR : referenceLengths) {
+        auto l = 100000;
+        if (l < lR) {
 
-    if ((*r)[length - 3] != 'T'){
-        (*r)[length - 3] = 'T';
-    } else {
-        (*r)[length - 3] = 'A';
+            auto pos = rand() % l;
+            std::cout << "Pos: " << pos << std::endl;
+            auto startPos = rand() % (lR - l);
+            *r = reference->substr(startPos, l);
+
+            if ((*r)[pos] != 'T'){
+                (*r)[pos] = 'T';
+            } else {
+                (*r)[pos] = 'A';
+            }
+
+            std::cout << "Reference length: " << lR << std::endl;
+            std::cout << "r length: " << l << std::endl;
+
+
+            auto begin = std::chrono::steady_clock::now();
+            size_t position = algorithms::findSNPPosition(reference, r, k, firstK, bloomFilterThreshold,
+                                                          firstThreshold);
+            auto end = std::chrono::steady_clock::now();
+
+            if (position == -1) {
+                std::cout << "SNP not found." << std::endl;
+            } else {
+                std::cout << "Found SNP at r position " << position << std::endl;
+            }
+            assert(position == pos);
+            std::cout << "Run matching in " <<
+            (double) std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / 1000 <<
+            " seconds" << std::endl;
+
+            file << l << "," << lR << "," << (double) std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / 1000 << "\n";
+            *reference = *originalReference;
+            *r = *originalRead;
+        }
     }
 
-    std::cout << "Reference length: " << reference->length() << std::endl;
-    std::cout << "r length: " << r->length() << std::endl;
+    auto newFile = std::ofstream("timingsRead.csv");
+    newFile << "read,reference,time\n";
+    for (auto l : readLengths) {
+        auto lR = reference->length();
+        if (l < lR) {
+
+            auto pos = rand() % l;
+            std::cout << "Pos: " << pos << std::endl;
+            auto startPos = rand() % (lR - l);
+            *r = reference->substr(startPos, l);
+
+            if ((*r)[pos] != 'T'){
+                (*r)[pos] = 'T';
+            } else {
+                (*r)[pos] = 'A';
+            }
+
+            std::cout << "Reference length: " << lR << std::endl;
+            std::cout << "r length: " << l << std::endl;
 
 
-    auto begin = std::chrono::steady_clock::now();
-    size_t position = algorithms::findSNPPosition(reference, r, k, firstK, bloomFilterThreshold,
-                                                  firstThreshold);
-    auto end = std::chrono::steady_clock::now();
+            auto begin = std::chrono::steady_clock::now();
+            size_t position = algorithms::findSNPPosition(reference, r, k, firstK, bloomFilterThreshold,
+                                                          firstThreshold);
+            auto end = std::chrono::steady_clock::now();
 
-    if (position == -1) {
-        std::cout << "SNP not found." << std::endl;
-    } else {
-        std::cout << "Found SNP at r position " << position << std::endl;
+            if (position == -1) {
+                std::cout << "SNP not found." << std::endl;
+            } else {
+                std::cout << "Found SNP at r position " << position << std::endl;
+            }
+            assert(position == pos);
+            std::cout << "Run matching in " <<
+                      (double) std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / 1000 <<
+                      " seconds" << std::endl;
+
+            newFile << l << "," << lR << "," << (double) std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / 1000 << "\n";
+
+        }
     }
-    std::cout << "Run matching in " << (double) std::chrono::duration_cast<std::chrono::milliseconds>(end -
-                                                                begin).count() / 1000 << " seconds" << std::endl;
+
+
     assert(position == length - 3);
 
     *reference = "RRAACTGTACGGGGGGGGCAAGTGCAAAAAAAAATAAAAAAAA";
     *r = "GGGGGGGGCAAGTGTAAAAAAAAATA";
-    position = algorithms::findSNPPosition(reference, r, 5, 5, 0.1, 0.7);
+    size_t position = algorithms::findSNPPosition(reference, r, 5, 5, 0.1, 0.7);
     assert(position == 14);
 
     *reference = "GGGAACGTAATTGCGGGTC";
